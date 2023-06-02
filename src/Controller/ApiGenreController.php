f6d7e5b3d805cd3f516cd7f26a6b3f875dc8c501
CAMARA <88945535+camara92@users.noreply.github.com>
2023-06-02 02:22:18 +0200

    create genre

--- src/Controller/ApiGenreController.php
+++ src/Controller/ApiGenreController.php
@@ -2,27 +2,80 @@
 
 namespace App\Controller;
 
+use App\Entity\Genre;
 use App\Repository\GenreRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ApiGenreController extends AbstractController
 {
-    #[Route('/api/genres', name: 'api_genres')]
+    #[Route('/api/genres', name: 'api_genres',  methods: 'GET')]
     public function listeGenre(GenreRepository $genreRepository, SerializerInterface $serializer): Response
     {
         $genres = $genreRepository->findAll();
         $result = $serializer->serialize($genres, 'json', [
-            'groups' => [ 'listGenreFull']
+            'groups' => ['listGenreFull']
         ]);
         // return $this->render('api_genre/index.html.twig', [
         //     'controller_name' => 'ApiGenreController',
         // ]);
         // true : permet de savoir si serialiser sinon le faire en format que l'on veut 
-        return new JsonResponse($result, 200, [], true );
+        return new JsonResponse($result, 200, [], true);
+    }
+
+    #[Route('/api/genres/{id}', name: 'api_genres_show', methods: 'GET')]
+    public function ShowGenre(Genre $grenre,  SerializerInterface $serializer): Response
+    {
+
+        $result = $serializer->serialize($grenre, 'json', [
+            'groups' => ['listGenreSimple']
+        ]);
+
+        return new JsonResponse($result, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/genres/', name: 'api_genres_create', methods: 'POST')]
+    public function CreateGenre(Request $request,  SerializerInterface $serializer,  EntityManagerInterface $manager): Response
+    {
+
+        $data = $request->getContent();
+        // $genre = new Genre();
+
+        // $serializer = $serializer->deserialize($data, Genre::class, 'json', [
+        //     'object_to_populate' => $genre
+        // ]);
+        //methode 2 : 
+        $genre = $serializer->deserialize($data ,Genre::class, 'json');
+       
+
+        $manager->persist($genre);
+        $manager->flush();
+
+       
+
+
+
+
+        return new JsonResponse(
+            
+            'Le nouveau genre a bien été crée. Merci Daouda',
+            Response::HTTP_CREATED, [
+            'location' => 'api/genres/' . $genre->getId()
+            // renvoyer la nouvelle header 
+        ], true);
+
+        //   ['location'=>$this->generateUrl('api/genres_show', ['id'=>$genre->getId(), UrlGeneratorInterface::ABSOLUTE_URL])];
+
+
     }
 }
